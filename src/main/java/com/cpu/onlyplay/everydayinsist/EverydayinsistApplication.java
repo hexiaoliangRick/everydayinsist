8a96c417b36472d94ba16300e2ba5de56ae7f989
hexiaoliang <hexiaoliangrick@outlook.com>
2022-08-11 21:09:35 +0800

    'init'

---
+++ src/main/java/com/cpu/onlyplay/everydayinsist/EverydayinsistApplication.java
@@ -0,0 +1,13 @@
+package com.cpu.onlyplay.everydayinsist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverydayinsistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EverydayinsistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cpu/onlyplay/everydayinsist/EverydayinsistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpu.onlyplay.everydayinsist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverydayinsistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
